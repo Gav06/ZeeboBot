173d1f457d1315ff565fca6b330a4a91b3b19245
Gavin <72846934+Gav06@users.noreply.github.com>
2025-04-02 19:46:41 -0400

    ping pong

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.DirectMessages
     ]
 })
 
-client.login(process.env.DISCORD_TOKEN)
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(client.guilds.cache.map(guild => guild.name));
+});
 
-client.on("messageCreate", async (message) => {
+client.on("messageCreate", (message) => {
     console.log(message)
 
-    if (!message?.author.bot) {
-        message.author.send(`Echo ${message.content}`)
+    if (message.content.toLowerCase() === 'ping' && !message.author.bot) {
+        message.channel.send(`Pong! :ping_pong:`)
     }
 })
+
+client.login(process.env.DISCORD_TOKEN)
